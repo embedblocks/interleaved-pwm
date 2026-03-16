ff8594b47cdf8272ce5a78449c2a6a2766cd50a6
khiyam <khiyamiftikhar@gmail.com>
2026-03-16 20:34:44 +0500

    feat: created, runs all tests, then shows menu

--- test_app/main/main_test.c
+++ test_app/main/main_test.c
@@ -7,49 +7,8 @@
 
 void app_main(){
 
-/*
-UNITY_BEGIN();
-    unity_run_test_by_name("PoolManager Create Object");
-    unity_run_test_by_name("PoolManager Allocate object1");
-    unity_run_test_by_name("PoolManager Allocate object2");
-    unity_run_test_by_name("PoolManager Allocate object3");
-    unity_run_test_by_name("PoolManager Allocate object4");
-
-    UNITY_END();
-    
-    
-    UNITY_BEGIN();
-    unity_run_test_by_name("PoolManager Create Object");
-    unity_run_test_by_name("PoolManager Allocate object1");
-    unity_run_test_by_name("PoolManager Allocate object2");
-    unity_run_test_by_name("PoolManager Allocate object3");
-    unity_run_test_by_name("PoolManager Allocate object4");
-
-    unity_run_test_by_name("PoolManager Deallocate object1");
-
-    unity_run_test_by_name("PoolManager Allocate object4");
-
-    
-
-    UNITY_END();
-    
-    
-    UNITY_BEGIN();
-    unity_run_test_by_name("PoolManager Create Object");
-    unity_run_test_by_name("PoolManager Allocate All 4");
-    
-
-    UNITY_END();
-    
-    
-    
-  */  
-    
-    
-    
-    
-    
-    
+  
+  unity_run_all_tests();
 
   while(1){
 
